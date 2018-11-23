ac845e1b31ff6493955f6a55936b0ca25875e91f
jellyforjully088@yahoo.com <jellyforjully088@yahoo.com>
2018-11-24 00:39:41 +0100

    test

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Aux'
+class BurgerBulder extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Aux>
+            <div>Burger</div>
+            <div>Build Control</div>
+            </Aux>
+
+         );
+    }
+}
+ 
+export default BurgerBulder;
